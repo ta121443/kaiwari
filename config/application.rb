7c46c2d34806cf0aa5990ebf15ba729c2a660eb7
ta121443 <ryo_uchi4_20@outlook.jp>
2022-11-30 15:02:51 +0000

    erbをslimに変更

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,6 @@ module Myapp
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.generators.template_engine = :slim #slimに変更
   end
 end
