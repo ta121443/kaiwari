a893911a7d2101b7a90a184705937241b6ceedbb
root <ryo_uchi4_20@outlook.jp>
2022-12-06 07:57:06 +0000

    総計アルゴリズム完了

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -8,9 +8,12 @@ class CalculationsController < ApplicationController
 
     @users = User.where('session_id = ?', params[:session_id])
     @user_num = User.where('session_id = ?', params[:session_id]).count
-    graph, @remainder = make_graph(histories)
-    @payment = {}
-    # min_cash_flow(graph)
+    @graph = make_graph(histories)
+    @payment = []
+    min_cash_flow(@graph)
+    # @user_num = 3
+    graph = [ [0, 1000, 2000], [0, 0, 5000], [0, 0, 0] ]
+    @after = make_after(@graph)
   end
 
   private
@@ -20,7 +23,17 @@ class CalculationsController < ApplicationController
       return x < y ? x : y
     end
 
-    
+    def make_after(graph)
+      amount = Array.new(@user_num, 0)
+      for p in 0...@user_num
+        for i in 0...@user_num
+          amount[p] += (graph[i][p] - graph[p][i])
+        end
+      end
+      return amount
+    end
+
+    # 総額を計算
     def calculate_total(histories)
       total = 0
       histories.each do | history |
@@ -31,11 +44,28 @@ class CalculationsController < ApplicationController
 
     # 誰が誰にいくら払うかを示す配列を作成
     def make_graph(histories)
-      graph = Array.new(@user_num) { Array.new(@user_num, 0) }
-      remainder = 0
-    #   histories.each do | history |
-    #   end
-      return graph, remainder
+      graph = Array.new(@user_num) { Array.new(@user_num, 0) }    # 誰が誰にいくら払うか
+
+      histories.each do | history |
+        price = history.price
+        paid_user = history.user_id - 1   # 支払ったメンバーのidを取得し、indexに使うため-1する
+
+        # 割り切れない分は支払い者が得するように
+        while price % @user_num != 0 do
+          price += 1
+        end
+
+        per_one = price / @user_num   # 一人当たりの金額
+
+        for i in 0...@user_num
+          if i == (paid_user)
+            next
+          end
+          graph[i][paid_user] += per_one
+        end
+      end
+
+      return graph
     end
 
     def min_cash_flow(graph)
@@ -62,8 +92,7 @@ class CalculationsController < ApplicationController
       amount[mx_credit] -= min
       amount[mx_debit] += min
 
-      @payment[mx_debit] = [mx_credit, min]
-
+      @payment.push([mx_debit, [mx_credit, min]])
       min_cash_flow_rec(amount)
     end
 end
